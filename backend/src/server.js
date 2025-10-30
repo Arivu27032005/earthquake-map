dbc468d8537925cfb2036e7477c4eaf135d7db6c
Arivu L <arivalaganlak@gmail.com>
2025-10-30 20:16:41 +0530

    completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'; 
 import axios from 'axios';
+import {fileURLToPath} from 'url';
+import path from 'path';
 
 // Load environment variables from .env
 dotenv.config();
@@ -10,16 +12,23 @@ dotenv.config();
 // Create an Express app
 const app = express();
 
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
 // Set server port from environment variable or default to 5000
 const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
-// Enable CORS for frontend running on localhost:5173
-app.use(cors({
-    origin: 'http://localhost:5173' // Allow requests from this origin
-}));
+// Enable CORS for all routes
+app.use(cors());
+
+app.use(express.static(path.join(_dirname, '.../frontend/dist')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(_dirname, '../frontend/dist/index.html'));
+});
 
 // API route to fetch earthquake data from USGS
 app.get('/api/earthquakes', async (req, res) => {
