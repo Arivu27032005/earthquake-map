b338017ff85d43317082a618949e2762c5dff583
Arivu L <arivalaganlak@gmail.com>
2025-10-30 21:08:42 +0530

    completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,9 +24,9 @@ app.use(express.json());
 // Enable CORS for all routes
 app.use(cors());
 
-app.use(express.static(path.join(_dirname, '.../frontend/dist')));
+app.use(express.static(path.join(_dirname, '../frontend/dist')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(_dirname, '../frontend/dist/index.html'));
 });
 
